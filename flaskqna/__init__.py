7f00ddf56d58d6d0211b68e0ca158d5849411e94
Ovicron <Ovicrondw@gmail.com>
2020-07-24 19:13:51 -0400

    db

--- flaskqna/__init__.py
+++ flaskqna/__init__.py
@@ -16,14 +16,14 @@ bcrypt = Bcrypt(app)
 # DATABASE CONFIGS ---------------------------------------------------------------------------
 db = SQLAlchemy(app) 
 
-ENV = 'dev'
+ENV = 'prod'
 
 if ENV == 'dev':
     app.debug = True
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///qna.db'
 else:
     app.debug = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://eeeipoewygqnbe:33221acb0b74a9eb64bb0d09c3655afcb7d112be096b498c2701a476b0c416b7@ec2-50-19-26-235.compute-1.amazonaws.com:5432/d1h6g66ju650eq'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
