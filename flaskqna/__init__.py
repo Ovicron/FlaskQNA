6fc02d3bf36980f247340cf82da246620050b5e9
Ovicron <40534384+Ovicron@users.noreply.github.com>
2020-12-19 20:50:05 -0500

    Update __init__.py

--- flaskqna/__init__.py
+++ flaskqna/__init__.py
@@ -23,7 +23,7 @@ if ENV == 'dev':
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///qna.db'
 else:
     app.debug = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://eeeipoewygqnbe:33221acb0b74a9eb64bb0d09c3655afcb7d112be096b498c2701a476b0c416b7@ec2-50-19-26-235.compute-1.amazonaws.com:5432/d1h6g66ju650eq'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://TRIPLE_E_HEREipoewygqnbe:33221acb0b74a9eb64bb0d09c3655afcb7d112be096b498c2701a476b0c416b7@ec2-50-19-26-235.compute-1.amazonaws.com:5432/d1h6g66ju650eq'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
